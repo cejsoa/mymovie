3c44f940fef2d8ef62bafb5a742b646416012194
Crisptofer Fernández Fernández <alberto12ff@gmail.com>
2020-11-15 23:48:23 -0600

    Avg and dummy tests added

---
+++ app/tests/avgGrade.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest')
+const app = require('../../server')
+describe('calcAvgGrade', () => {
+  it('should calculate average grade of a movie', async () => {
+    const res = await request(app)
+      .get('/api/movies/calcavggrade/0')
+    expect(res.statusCode).toEqual(200)
+    expect(typeof res.body.AvgGrade).toBe('number')
+  })
+})
