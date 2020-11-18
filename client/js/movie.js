f8c7a81e6d1fd1dcd29584aac994e7d99d08756d
Jose Ortega <37756558+ortegajosant@users.noreply.github.com>
2020-11-18 14:20:56 -0600

    Modify movie feature added (#21)
    Co-authored-by: Jose Ortega <jose.ortega@schoolmint.net>

--- client/js/movie.js
+++ client/js/movie.js
@@ -24,7 +24,9 @@ const render_movie = (item) => {
     fetch("/api/styles/findOne/" + item.IdStyle)
         .then(response => response.json())
         .then(data => document.getElementById("movie-style").innerText = data.Style_Name);
-
+    fetch("/api/images/findOne/" + item.IdImage)
+        .then(response => response.json())
+        .then(data => document.getElementById("movie-image-src").src = data.Image_Link);
 }
 
 const render_comments = (item) => {
@@ -69,8 +71,6 @@ function get_movie_info() {
         .then(data => fill_comments(data));
 }
 
-get_movie_info();
-
 function post_comment() {
     let id = window.location.href.split('/');
     id = id[id.length - 1];
@@ -93,3 +93,90 @@ function post_comment() {
             .then(() => append_comment(item));
     }
 }
+
+const send_modified_values = async (movie, image) => {
+    let id_movie = window.location.href.split("/");
+    id_movie = id_movie[id_movie.length - 1];
+
+    console.log(id_movie);
+
+    if (id_movie != undefined || id_movie != null) {
+        await fetch("/api/images/create",
+            {
+                method: 'POST',
+                body: JSON.stringify(image),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(response => response.json())
+            .catch(error => console.error('Error:', error))
+            .then(data => movie["IdImage"] = data.Id);
+        
+        console.log(movie);
+        await fetch("/api/movies/update/" + id_movie,
+            {
+                method: 'PUT',
+                body: JSON.stringify(movie),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(response => response.json())
+            .catch(error => console.error('Error:', error))
+            .then(data => console.log(data));
+    }
+}
+
+function modify_movie() {
+    let movie_name = document.getElementById("movie-name-id-modify").value.trim();
+    let movie_director = document.getElementById("movie-director-id-modify").value.trim();
+    let movie_genre = document.getElementById("movie-genre-id-modify").value.trim();
+    let movie_lang = document.getElementById("movie-lang-id-modify").value.trim();
+    let movie_style = document.getElementById("movie-style-id-modify").value.trim();
+    let movie_fav = document.getElementById("movie-fav-id-modify").value.trim();
+    let movie_year = document.getElementById("movie-year-id-modify").value.trim();
+    let movie_imdb = document.getElementById("movie-imdb-id-modify").value.trim();
+    let movie_metascore = document.getElementById("movie-metascore-id-modify").value.trim();
+    let movie_image = document.getElementById("movie-image-id-modify").value.trim();
+
+    let movie = {};
+
+    let image = {};
+
+    if (movie_name !== "") {
+        movie["NameMovie"] = movie_name;
+    }
+    if (movie_director !== "") {
+        movie["NameDirector"] = movie_director;
+    }
+    if (movie_year !== "") {
+        movie["Year_M"] = movie_year;
+    }
+
+    if (movie_image !== "") {
+        image["Image_Link"] = movie_image;
+    }
+    if (movie_genre !== "") {
+        movie["IdGenre"] = movie_genre;
+    }
+    if (movie_lang !== "") {
+        movie["IdLanguage"] = movie_lang;
+    }
+    if (movie_fav !== "") {
+        movie["Favorite"] = movie_fav;
+    }
+    if (movie_imdb !== "") {
+        movie["IMDBGrade"] = movie_imdb;
+    }
+    if (movie_style !== "") {
+        movie["IdStyle"] = movie_style;
+    }
+    if (movie_metascore !== "") {
+        movie["MetaScoreGrade"] = movie_metascore;
+    }
+
+    send_modified_values(movie, image);
+}
+
+get_movie_info();
